c622cf5612a5d77964691c5534e782816621faea
dmqrs77 <dmqrs240993@gmail.com>
2025-11-10 21:40:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram import Bot
 from telegram.ext import ApplicationBuilder, CommandHandler
+import asyncio
 
 TOKEN = "SEU_TOKEN_AQUI"
 CHAT_ID = "SEU_CHAT_ID_AQUI"
@@ -11,15 +12,11 @@ async def main():
     print("✅ Bot iniciado... aguardando mensagens.")
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
-
     await application.run_polling()
 
-if _name_ == "_main_":
-    import asyncio
+if __name__ == "__main__":
     try:
-        # Tenta criar e usar o novo loop explicitamente (compatível com Python 3.13)
         asyncio.run(main())
     except RuntimeError:
-        # Se já houver um loop rodando, usa o existente
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main())
